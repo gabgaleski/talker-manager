c96af302654333da96cd3db9ef38b4aa37dbe01c
Gabriel Galeski <galeski35@gmail.com>
2023-06-19 16:42:49 -0300

    Realiza requisito 4 refatorado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const talkerRoute = require('./routes/talkerRoute');
-const loginRoute = require('./routes/loginRoute');
+const readAndWriteFile = require('./utils/readAndWriteFile');
+const generateToken = require('./utils/generateToken');
+const loginValidation = require('./middleware/loginValidation');
 
 const app = express();
 app.use(express.json());
@@ -13,8 +14,25 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.use('/talker', talkerRoute);
-app.use('/login', loginRoute);
+app.get('/talker', async (_req, res) => {
+  const talkers = await readAndWriteFile.readFile();
+  if (!talkers) return res.status(200).json([]);
+  return res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readAndWriteFile.getById(id);
+  if (!talker) {
+return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+}
+  return res.status(200).json(talker);
+});
+
+app.post('/login', loginValidation, async (_req, res) => {
+  const getToken = await generateToken();
+  return res.status(200).json({ token: getToken });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
